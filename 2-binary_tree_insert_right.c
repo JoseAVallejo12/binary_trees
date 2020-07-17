7153531a3e6267f72b20c8df42620fc34a60bcfb
andreshugueth <andreshugueth@gmail.com>
2020-07-17 15:48:04 -0500

    2nd task and betty check 1&2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - funtion for insert node left side
+ * binary_tree_insert_lefts - funtion for insert node left side
  * @parent: node to insert children left side
  * @value: valor of node left to insert
  * Return: pointer to new node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
@@ -13,15 +13,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new_node == NULL || parent == NULL)
 		return (NULL);
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
 		new_node->parent = parent;
-		parent->left->parent = new_node;
-		new_node->left = parent->left;
-		parent->left = new_node;
+		parent->right->parent = new_node;
+		new_node->right = parent->right;
+		parent->right = new_node;
 	}
 	else
-		parent->left = new_node;
+		parent->right = new_node;
 
 	return (parent);
 }
