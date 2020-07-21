6c6a5fee50cbc21df2b4320c97f3cb25826901bc
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-21 18:08:52 -0500

    update task 13 and fix some problems

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,33 +1,17 @@
 #include "binary_trees.h"
-
-int travel_tree(const binary_tree_t *tree, size_t n_l, size_t n_r)
-{
-	if (tree == NULL)
-		return (1);
-
-	n_l += travel_tree(tree->left, n_l, n_r);
-	n_r += travel_tree(tree->right, n_l, n_r);
-
-	if (n_l > n_r)
-		return n_l;
-	else
-		return n_r;
-}
-
-
 /**
- * binary_tree_leaves - counts the leaves in a binary tree
- * @tree:  pointer to the root node of the tree to count the number of leaves
+ * binary_tree_nodes - counts number the node with almost one children
+ * @tree:  pointer to the root node of the tree
  * Return: number of leaves
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t node_r, node_l;
-	node_r = 0;
-	node_l = 0;
 	if (tree == NULL)
 		return (0);
 
-	return(travel_tree(tree, node_l, node_r) - 1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
+	return (1 + binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right));
 }
