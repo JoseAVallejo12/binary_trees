9ef59d77345407eefe5d6bd539cada54903ab89e
andreshugueth <andreshugueth@gmail.com>
2020-07-21 14:11:08 -0500

    12th task

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 /**
- * traversal - post order traversal binary tree
- * @root: root of the analyzed tree
- * @cnt: counter of leaves
- * Return: number of leaves
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node:  pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0
  */
-size_t traversal(const binary_tree_t *root, size_t cnt)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!root)
+	if (node == NULL)
 		return (0);
-	if (traversal(root->left, cnt) == 0 && traversal(root->right, cnt) == 0)
-		cnt += 1;
-	return (cnt);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
 }
 /**
  * binary_tree_leaves - counts the leaves in a binary tree
@@ -20,15 +20,15 @@ size_t traversal(const binary_tree_t *root, size_t cnt)
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count, total;
+	size_t right_c, left_c;
 
-	count = total = 0;
 	if (!tree)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
+	if (tree != NULL && binary_tree_is_leaf(tree))
 		return (1);
 
-	total = traversal(tree, count);
-	return (total + 1);
+	left_c = binary_tree_leaves(tree->left);
+	right_c = binary_tree_leaves(tree->right);
+	return (left_c + right_c);
 }
