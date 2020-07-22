dd5329dd23c51688f29ceaddf71bc4ea7b317056
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-21 19:19:20 -0500

    more fix output task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,37 +1,36 @@
 #include "binary_trees.h"
 /**
- * _is_a_leaf - checks if a node is a leaf
- * @node:  pointer to the node to check
- * Return: 1 if node is a leaf, otherwise 0
+ * traversal_p - post order traversal binary tree
+ * @root: root of the analyzed tree
+ * @left_c: counter to the left
+ * @right_c: counter to the right
+ * Return: height of the binary tree
  */
-int _is_a_leaf(const binary_tree_t *node)
+int traversal_p(const binary_tree_t *tree, int left_c, int right_c)
 {
-	if (node == NULL)
-		return (0);
-	if (node->left == NULL && node->right == NULL)
-		return (1);
-	else
-		return (0);
-}
-/**
- * height - counts the leaves in a binary tree
- * @tree:  pointer to the root node of the tree to count the number of leaves
- * Return: number of leaves
- */
-
-size_t height(const binary_tree_t *tree)
-{
-	size_t right_c, left_c;
-
 	if (!tree)
 		return (0);
+	left_c = traversal_p(tree->left, left_c, right_c) + 1;
+	right_c = traversal_p(tree->right, left_c, right_c) + 1;
+	if (right_c > left_c)
+		return (right_c);
+	return (left_c);
+}
 
-	if (tree != NULL && _is_a_leaf(tree))
-		return (1);
+/**
+ * height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of the tree
+ */
+size_t height(const binary_tree_t *tree)
+{
+	int left_c, right_c, height;
 
-	left_c = height(tree->left);
-	right_c = height(tree->right);
-	return (left_c + right_c);
+	left_c = right_c = 0;
+	if (!tree)
+		return (0);
+	height = traversal_p(tree, left_c, right_c);
+	return (height);
 }
 
 /**
@@ -39,18 +38,13 @@ size_t height(const binary_tree_t *tree)
  * @tree:  pointer to the root node of the tree to count the number of leaves
  * Return: balancer
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int h_l, h_r;
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	h_l = height(tree->left);
-	h_r = height(tree->right);
-
-	return (h_l - h_r);
+	return (height(tree->left) - height(tree->right));
 }
